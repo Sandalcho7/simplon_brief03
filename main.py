a78c7cf5411c3018b9cd51079d45dd4a2a454c0d
Sandalcho7 <bahurel.anatole@gmail.com>
2024-01-11 16:20:33 +0100

    update after code review by Olivier

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 # Imports
 from fastapi import FastAPI, HTTPException, Path
 import sqlite3
+import yaml
 
+# Import du fichier YAML : config du projet
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 
+# Connexion à la base de données
+database_url = config.get('database', {}).get('url')
+if database_url:
+    con = sqlite3.connect(database_url)
+    print(f"Connexion à la base de données avec l'URL : {database_url}")
+else:
+    print("Aucune URL de base de données spécifiée dans le fichier de configuration.")
 
 # Déclaration des variables
-con = sqlite3.connect("immo_fr.db")
-
 app = FastAPI()
-
-
 
 # Déclaration des fonctions
 def validate_year(year: str):
@@ -28,19 +35,16 @@ def execute_sql(con, query):
     return result_with_columns
 
 
-
 # Root
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
 
 # User story 1
 @app.get("/revenu_fiscal_moyen/{city}", description="Renvoie le revenu fiscal moyen de l'année la plus récente pour une ville donnée")
 async def revenu_fiscal_moyen(city: str = Path(description="Ville")):
     query = f"SELECT revenu_fiscal_moyen FROM foyers_fiscaux ff WHERE ville = '{city.capitalize()}' ORDER BY date DESC LIMIT 1"
     return execute_sql(con, query)
-
 
 # User story 2
 @app.get("/dernieres_transactions/{city}_last_{number}", description="Renvoie le nombre choisi de dernières transactions pour une ville donnée")
@@ -49,7 +53,6 @@ async def last_transactions(city: str = Path(description="Ville"),
     query = f"SELECT * FROM transactions_sample ts WHERE ville LIKE '{city.upper()}%' ORDER BY date_transaction DESC LIMIT {number}"
     return execute_sql(con, query)
 
-
 # User story 3
 @app.get("/nombre_transactions/{city}_{year}", description="Renvoie le nombre de transactions pour une ville et une année donnés")
 async def transactions_count(city: str = Path(description="Ville"),
@@ -57,7 +60,6 @@ async def transactions_count(city: str = Path(description="Ville"),
     year = validate_year(year)
     query = f"SELECT COUNT(*) as nb_transactions FROM transactions_sample ts WHERE ville LIKE '{city.upper()}%' AND date_transaction LIKE '{year}%'"
     return execute_sql(con, query)
-        
 
 # User story 4
 @app.get("/prix_moyen_m2/{year}_{type}", description="Renvoie le prix moyen par m2 pour une année et un type de biens donnés")
@@ -66,7 +68,6 @@ async def average_price(type: str = Path(description="Type de biens"),
     year = validate_year(year)
     query = f"SELECT AVG(prix/surface_habitable) as prix_m2_moyen FROM transactions_sample ts WHERE type_batiment = '{type.capitalize()}' AND date_transaction LIKE '{year}%'"
     return execute_sql(con, query)
-
 
 # User story 5
 @app.get("/nombre_transactions_2/{city}_{year}_{type}_{rooms}", description="Renvoie le nombre de transactions pour une ville, une année, un type de biens et un nombre de pièces donnés")
@@ -78,7 +79,6 @@ async def transactions_count2(city: str = Path(description="Ville"),
     query = f"SELECT COUNT(*) as nb_transactions FROM transactions_sample ts WHERE type_batiment = '{type.capitalize()}' AND n_pieces = {rooms} AND ville LIKE '{city.upper()}%' AND date_transaction LIKE '{year}%'"
     return execute_sql(con, query)
 
-
 # User story 6
 @app.get("/repartition_pieces/{city}_{year}_{type}", description="Renvoie la répartition des transactions en fonction du nombre de pièces, pour une ville, une année et un type de biens donnés")
 async def room_repart(city: str = Path(description="Ville"),
@@ -87,7 +87,6 @@ async def room_repart(city: str = Path(description="Ville"),
     year = validate_year(year)
     query = f"SELECT n_pieces, COUNT(*) as nb_transactions FROM transactions_sample ts WHERE type_batiment = '{type.capitalize()}' AND ville LIKE '{city.upper()}%' AND date_transaction LIKE '{year}%' GROUP BY n_pieces"
     return execute_sql(con, query)
-
 
 # User story 7                                                                                                                                                                                                   
 @app.get("/prix_moyen_m2_2/{city}_{year}_{type}", description="Renvoie le prix moyen par m2 pour une ville, une année et un type de biens donnés")
@@ -98,13 +97,11 @@ async def average_price_2(city: str = Path(description="Ville"),
     query = f"SELECT AVG(prix/surface_habitable) as prix_m2_moyen FROM transactions_sample ts WHERE type_batiment = '{type.capitalize()}' AND ville LIKE '{city.upper()}%' AND date_transaction LIKE '{year}%'"
     return execute_sql(con, query)
 
-
 # User story 8
 @app.get("/departement_transactions/", description="Renvoie le classement des départements en fonction du nombre de transactions réalisées")
 async def dep_transactions():
     query = f"SELECT departement, COUNT(*) as nb_transactions FROM transactions_sample ts GROUP BY departement ORDER BY nb_transactions DESC"
     return execute_sql(con, query)
-
 
 # User story 9
 @app.get("/nombre_transactions_3/{year1}_{revenu}_{year2}_{type}", description="Renvoie le nombre total de ventes d'un type de biens à une année donnée, pour les villes ayant un revenu fiscal moyen supérieur à la valeur renseignée au cours de l'année choisie")
@@ -115,15 +112,13 @@ async def transactions_count3(year1: str = Path(description="Année concernant l
     query = f"SELECT COUNT(*) as nb_transactions FROM transactions_sample ts JOIN foyers_fiscaux ff ON ts.ville = UPPER(ff.ville) WHERE ff.date LIKE '{year1}%' AND ff.revenu_fiscal_moyen > {revenu} AND ts.date_transaction LIKE '{year2}%' AND type_batiment = '{type}'"
     return execute_sql(con, query)
 
-
 # User story 10
 @app.get("/cities_top10_transactions/", description="Renvoie les 10 villes avec le plus de transactions immobilières recensées")
 async def cities_top10():
     query = f"SELECT ville, COUNT(*) as nb_transactions FROM transactions_sample ts GROUP BY ville ORDER BY COUNT(*) DESC LIMIT 10"
     return execute_sql(con, query)
 
-
-# User story 11 & 12
+# User stories 11 & 12
 @app.get("/cities_top10_price/{type}", description="Renvoie les 10 villes avec le prix moyen au m2 le plus attractif pour un type de biens donné")
 async def cities_top10_price(type: str = Path(description="Type de biens")):
     query = f"SELECT ville, AVG(prix/surface_habitable) as prix_m2_moyen FROM transactions_sample ts WHERE type_batiment = '{type.capitalize()}' GROUP BY ville ORDER BY prix_m2_moyen ASC LIMIT 10"
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# SIMPLON DEV IA | Brief 3
+# SIMPLON DEV IA | Brief 3 (09/01/24)
 
 ## FastAPI - exposer les données de transactions immobilières via une API
 
@@ -8,7 +8,10 @@ Rendu du brief en semaine 3 introduisant aux principes d'API et API REST en util
 
 ### Prérequis
 
-Avant de démarrer le développement le projet, il est nécessaire d'installer FastAPI et Uvicorn sur l'environnement de travail.
+Avant de démarrer le développement le projet, il est nécessaire d'installer FastAPI, Uvicorn, SQLite et PyYAML sur l'environnement de travail. Pour effectuer ces installations, vous pouvez éxécuter la commande suivante :
+```bash
+pip install -r ressources/requirements.txt
+```
 
 ### Data
 
@@ -23,3 +26,116 @@ Avant de démarrer le développement le projet, il est nécessaire d'installer F
 ### Notes
 
 Certaines requêtes SQL sont légèrement différentes des user stories fournies, par l'ajout de paramètres, je les ai rendues plus génériques et donc utilisables dans de plus nombreux cas (différentes années, villes, etc.).
+
+### Doc
+
+Pour un hôte (127.0.0.1) et un port (8000), accédez à http://127.0.0.1:8000/docs sur votre navigateur, une fois le serveur lancé, pour accéder aux fonctionnalités de l'API.
+
+<hr>
+
+User story 1 :
+URL: /revenu_fiscal_moyen/{city}
+    Method: GET
+    URL Params:
+        city=[string] (required)
+    Success Response: JSON object with average revenue for the selected city.
+    Error Response:
+        400 Bad Request if null response
+
+User story 2 :
+URL: /dernieres_transactions/{city}_last_{number}
+    Method: GET
+    URL Params:
+        city=[string] (required)
+        number=[int] (required)
+    Success Response: JSON object with last transactions for the selected city, 'number' parameter selects the number of displayed transactions
+    Error Response:
+        400 Bad Request if null response
+
+User story 3 :
+URL: /nombre_transactions/{city}_{year}
+    Method: GET
+    URL Params:
+        city=[string] (required)
+        year=[string] (required)
+    Success Response: JSON object with transactions count for the selected city and year
+    Error Response:
+        400 Bad Request if null response
+
+User story 4 :
+URL: /prix_moyen_m2/{year}_{type}
+    Method: GET
+    URL Params:
+        city=[string] (required)
+        type=[string] (required)
+    Success Response: JSON object with average price per square meter for the selected city and building type
+    Error Response:
+        400 Bad Request if null response
+
+User story 5 :
+URL: /nombre_transactions_2/{city}_{year}_{type}_{rooms}
+    Method: GET
+    URL Params:
+        city=[string] (required)
+        year=[string] (required)
+        type=[string] (required)
+        rooms=[int] (required)
+    Success Response: JSON object with transactions count for selected city, year, building type and number of rooms
+    Error Response:
+        400 Bad Request if null response
+
+User story 6 :
+URL: /repartition_pieces/{city}_{year}_{type}
+    Method: GET
+    URL Params:
+        city=[string] (required)
+        year=[string] (required)
+    Success Response: JSON object with transactions repartition for the selected city, year and building type
+    Error Response:
+        400 Bad Request if null response
+
+User story 7 :
+URL: /prix_moyen_m2_2/{city}_{year}_{type}
+    Method: GET
+    URL Params:
+        city=[string] (required)
+        year=[string] (required)
+        type=[string] (required)
+    Success Response: JSON object with average price per square meter for the selected city, year and building type
+    Error Response:
+        400 Bad Request if null response
+
+User story 8 :
+URL: /departement_transactions/
+    Method: GET
+    Success Response: JSON object with departements ranking for transactions count
+    Error Response:
+        400 Bad Request if null response
+
+User story 9 :
+URL: /nombre_transactions_3/{year1}_{revenu}_{year2}_{type}
+    Method: GET
+    URL Params:
+        year1=[string] (required) Year of the average taxable income
+        revenu=[int] (required) Minimal average taxable income
+        year2=[string] (required) Year of the transactions count
+        type=[string] (required) Building type
+    Success Response: JSON object with transactions count for the selected year and building type, for the cities that had an average taxable income above the selected minimal income ('revenu') at a chosen year ('year2')
+    Error Response:
+        400 Bad Request if null response
+
+User story 10 :
+URL: /cities_top10_transactions/
+    Method: GET
+    Success Response: JSON object with the 10 cities that record the highest number of transactions
+    Error Response:
+        400 Bad Request if null response
+
+User stories 11 & 12 :
+URL: /cities_top10_price/{type}
+    Method: GET
+    URL Params:
+        type=[string] (required)
+    Success Response: JSON object with the 10 cities that have the lowest average price per square meter
+    Error Response:
+        400 Bad Request if null response
